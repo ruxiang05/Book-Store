ae3894decf5cb14360d4f8d29458ce1e81fd8024
Ruxandra Anghel <ruxiang05@gmail.com>
2017-09-24 17:25:43 +0100

    Configure store and provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 //Enables ES6 goodies like promises
 import 'babel-polyfill';
 import React from 'react';
+import { Provider } from 'react-redux';
 import { render } from 'react-dom';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import configureStore from './store/configureStore';
+
+const store = configureStore();
 
 render (
-    <Router routes={routes} history={browserHistory} />,
-    document.getElemenentById('app');
+    <Provider store={store}>
+        <Router routes={routes} history={browserHistory} />
+    </Provider>,
+    document.getElemenentById('app')
 );
