320c0e7465879b6340f852b293f77fbaebd41349
Ruxandra Anghel <ruxiang05@gmail.com>
2017-09-24 16:52:58 +0100

    Add index

--- src/index.js
+++ src/index.js
@@ -1,29 +1,12 @@
-/* eslint-disable import/default */
-
+//Enables ES6 goodies like promises
+import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import configureStore, { history } from './store/configureStore';
-import Root from './components/Root';
-import './styles/styles.scss'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-require('./favicon.ico'); // Tell webpack to load favicon.ico
-const store = configureStore();
+import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById('app')
+render (
+    <Router routes={routes} history={browserHistory} />,
+    document.getElemenentById('app');
 );
-
-if (module.hot) {
-  module.hot.accept('./components/Root', () => {
-    const NewRoot = require('./components/Root').default;
-    render(
-      <AppContainer>
-        <NewRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById('app')
-    );
-  });
-}
